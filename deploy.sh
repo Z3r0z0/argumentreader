7435fbd8f118840e0879c503fd9deadc751081b4
Tobias Mueller <Tobias.Mueller4@liebherr.com>
2020-06-17 14:48:04 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+
 dotnet nuget pack ArgumentsLib/bin/debug/CommandLineArgumentsLib.1.0.0.nupkg -Version $TEST_VERSION -Verbosity detailed && \
 dotnet nuget push ArgumentsLib/bin/debug/CommandLineArgumentsLib.1.0.0.nupkg $NUGET_ORG_KEY -Verbosity detailed -Source nuget.org
